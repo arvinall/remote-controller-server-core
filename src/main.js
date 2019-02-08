1e19485cf17911ebed8921634ada4fb36a21e184
Arvin Ladan <arvinall021@gmail.com>
2019-02-08 13:24:52 +0330

    Fix typo and importing method

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 
-const PACKAGE_JSON = require('../package')
+import packageJson from '../package'
 
 /**
  * Main Server Core class
  */
-export default class serverCore {
+export default class ServerCore {
   /**
    * Make the core ready
    *
    * @param {object} configs
-   * @param {string} [configs.mode='production'] (production|development) If set to development, developmentTool parameter attached to developmentServer start a new activity
+   * @param {string} [configs.mode='production'] (production|development) If set to development, development.tool attached to development.server and start a new activity
    */
   constructor (configs = {}) {
-    this.version = PACKAGE_JSON.version
+    this.version = packageJson.version
     this.mode = configs.mode || 'production'
   }
 }
