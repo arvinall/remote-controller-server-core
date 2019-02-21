be294eff19eb5c4fe6445269edbaecf692be2b91
Arvinall <arvinall021@gmail.com>
2019-02-21 23:17:15 +0330

    Add preferences module to main core module

--- src/main.js
+++ src/main.js
@@ -9,28 +9,37 @@
  * @see {@link https://nodejs.org/docs/latest-v10.x/api/events.html#events_class_eventemitter|EventEmitter}
  */
 
-import storages from './storages'
+import storagesMaker from './storages'
+import preferences from './preferences'
+
+var storages
 
 /**
  * core function creates main core module
  *
  * @param {object} [configs={}]
  * @param {string} [configs.storagePath=process.cwd()] Storages path address
+ * @param {string} [configs.preferencesStorageName='preferences']
  *
  * @return {module:remote-controller-server-core~core}
  */
-export default function coreMaker (configs = {}) {
+export default function coreMaker (configs = Object.create(null)) {
+  if (typeof configs !== 'object') throw new Error('configs parameter must be object')
+
+  // Set default configs
+  configs = Object.assign({
+    storagePath: process.cwd(),
+    preferencesStorageName: 'preferences'
+  }, configs)
+
   /**
    * @namespace module:remote-controller-server-core~core
    */
   const MODULE = Object.create(null)
 
-  if (typeof configs !== 'object') throw new Error('configs parameter must be object')
-
-  // Set default configs
-  configs = Object.assign({
-    storagePath: process.cwd()
-  }, configs)
+  if (storages === undefined || storages._path !== configs.storagePath) {
+    storages = storagesMaker({ path: configs.storagePath })
+  }
 
   /**
    * Storage manager module
@@ -40,7 +49,10 @@ export default function coreMaker (configs = {}) {
    *
    * @type {module:storages~Storages}
    */
-  MODULE.storages = storages({ path: configs.storagePath })
+  MODULE.preferences = preferences({
+    storages,
+    name: configs.preferencesStorageName
+  })
 
   return MODULE
 }
