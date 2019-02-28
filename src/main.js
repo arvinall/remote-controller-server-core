9338eb9fe4a2b386ce34990b7ed5ef0273a8658a
Arvinall <arvinall021@gmail.com>
2019-03-01 00:55:57 +0330

    Add Engine.io's methods as external to jsdoc

--- src/main.js
+++ src/main.js
@@ -5,8 +5,34 @@
 
 /**
  * Nodejs EventEmitter class
+ *
  * @external module:remote-controller-server-core~EventEmitter
  * @see {@link https://nodejs.org/docs/latest-v10.x/api/events.html#events_class_eventemitter|EventEmitter}
+ */
+/**
+ * Engine.io module
+ *
+ * @external module:remote-controller-server-core~engineIO
+ *
+ * @see {@link https://github.com/socketio/engine.io/blob/master/README.md|Engine.io's Github page}
+ */
+/**
+ * Engine.io's Socket class
+ *
+ * @name Socket
+ * @memberOf module:remote-controller-server-core~engineIO
+ * @static
+ *
+ * @see {@link https://github.com/socketio/engine.io/blob/master/README.md#socket|Engine.io's Github page}
+ */
+/**
+ * Engine.io's Server class
+ *
+ * @name Server
+ * @memberOf module:remote-controller-server-core~engineIO
+ * @static
+ *
+ * @see {@link https://github.com/socketio/engine.io/blob/master/README.md#server-2|Engine.io's Github page}
  */
 
 import storagesMaker from './storages'
