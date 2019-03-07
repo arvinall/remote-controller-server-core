a148dc172100fd7c68b5de104cc8ecf04b8f8ca7
Arvinall <arvinall021@gmail.com>
2019-03-07 22:48:31 +0330

    Make os mock more clear

--- src/engine/test/index_wihoutNet.test.js
+++ src/engine/test/index_wihoutNet.test.js
@@ -11,23 +11,23 @@ jest.mock('os', () => {
     [firstNetworkInterface]: [networkInterfacesCache[firstNetworkInterface][0]]
   })
 
-  return { networkInterfaces }
+  os.networkInterfaces = networkInterfaces
+
+  return os
 })
 
 const engine = engineMaker()
 
 describe('engine start method', () => {
-  describe('Errors', () => {
-    test('Must throw error when network is not available (async)', async () => {
-      expect.assertions(1)
+  test('Must throw error when network is not available (async)', async () => {
+    expect.assertions(1)
 
-      const ERROR = 'Network is not available'
+    const ERROR = 'Network is not available'
 
-      try {
-        await engine.start()
-      } catch (error) {
-        expect(error.message).toBe(ERROR)
-      }
-    })
+    try {
+      await engine.start()
+    } catch (error) {
+      expect(error.message).toBe(ERROR)
+    }
   })
 })
