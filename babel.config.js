f77a64f628f2522502f6daa5b396ec06969f75ea
Arvinall <arvinall021@gmail.com>
2019-03-08 19:54:34 +0330

    Remove polyfill from bundle

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,15 @@
 
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'usage',
-        modules: false
-      }
-    ]
+    [ '@babel/preset-env', { modules: false } ]
   ],
   plugins: [
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        corejs: false,
-        helpers: true,
-        regenerator: true,
-        useESModules: false
-      }
-    ],
+    [ '@babel/plugin-transform-runtime', {
+      corejs: false,
+      helpers: true,
+      regenerator: true,
+      useESModules: false
+    } ],
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-private-methods'
@@ -26,14 +17,8 @@ module.exports = {
   env: {
     test: {
       presets: [
-        [
-          '@babel/preset-env',
-          {
-            useBuiltIns: 'usage',
-            // Enable ES module system for jest
-            modules: 'auto'
-          }
-        ]
+        // Enable ES module system for jest
+        [ '@babel/preset-env', { modules: 'auto' } ]
       ]
     }
   }
