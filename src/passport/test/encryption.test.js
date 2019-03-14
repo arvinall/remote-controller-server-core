0c861d22273ce9f6c5ffc255c0f808fee4c91142
Arvinall <arvinall021@gmail.com>
2019-03-14 22:52:32 +0330

    Add encryption module tests

---
+++ src/passport/test/encryption.test.js
@@ -0,0 +1,27 @@
+/* global test, expect */
+
+import crypto from 'crypto'
+import * as passGenerator from './passGenerator'
+import encryption from '../encryption'
+
+test('Must throw error when password parameter is not string or characters are less than 2', () => {
+  const ERROR = 'password parameter is required and must be string with at least two character'
+
+  expect(encryption).toThrow(ERROR)
+  expect(encryption.bind(null, '')).toThrow(ERROR)
+  expect(encryption.bind(null, '1')).toThrow(ERROR)
+})
+
+test('Encrypt password without error', () => {
+  const salt = crypto.randomBytes(32)
+  const password = passGenerator.password(8)
+
+  let encryptedPass = encryption(password, salt)
+
+  expect(encryptedPass).toEqual(expect.objectContaining({
+    salt: expect.any(Buffer),
+    hash: expect.any(Buffer)
+  }))
+  expect(encryptedPass.salt.compare(salt)).toBe(0)
+  expect(encryptedPass.hash.length).toBe(salt.length * password.length)
+})
