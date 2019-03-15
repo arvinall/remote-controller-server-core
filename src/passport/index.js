bdbd8b579ba310b1d7c1c19c69e141ce697c9ab7
Arvinall <arvinall021@gmail.com>
2019-03-15 18:23:08 +0330

    Fix type

--- src/passport/index.js
+++ src/passport/index.js
@@ -29,14 +29,14 @@ export default class Passport {
    *
    * @param {string} type
    * @param {*} passportInput Depend on passport's type
-   * @param {{salt: Buffer, hash: Buffer}} passport
+   * @param {{salt: Buffer, hash: Buffer}} [passport]
    */
   constructor (type, passportInput, passport) {
     if (typeof type !== 'string' || !passports.hasOwnProperty(type)) {
       throw new Error('type parameter is required and must be string and one of passport types')
     } else if (passportInput === undefined) throw GLOBAL_ERRORS.passportInputRequired
     else if (passportInput === null && (typeof passport !== 'object' || !(passport.hash instanceof Buffer) || !(passport.hash instanceof Buffer))) {
-      throw new Error('passport parameter must be a passport object')
+      throw new Error('passport parameter must be a encryption object')
     }
 
     this.#type = type
@@ -50,7 +50,7 @@ export default class Passport {
   /**
    * Check passport equality
    *
-   * @param {*} passportInput This parameter can be a instance of Passport or passport[type] input
+   * @param {*} passportInput This parameter can be a encryption object or passport[type] input
    *
    * @return {boolean}
    */
