4f35e840cf2eba4a3534a76de5955ac03fa463ee
Arvinall <arvinall021@gmail.com>
2019-06-26 01:28:50 +0430

    Refactor class

--- src/logger.js
+++ src/logger.js
@@ -8,6 +8,8 @@ import fs from 'fs'
 import path from 'path'
 import { promisify } from 'util'
 
+const INDENT = 2 // spaces
+
 /**
  * Simple logger
  *
@@ -22,17 +24,28 @@ export default class Logger extends EventEmitter {
    * @type string
    */
   #errorPath
-  #logger = {
-    info: async message => {
-      if (typeof this.#infoPath !== 'string') return
+  /**
+   * @summary Append log to log files
+   * @description
+   * Available type:
+   * * `info`
+   * * `error`
+   *
+   * @param {string} type Target log file
+   * @param {*} data This parameter pass to JSON.stringify
+   *
+   * @return {Promise<(void|Error)>}
+   */
+  #append = async (type, data) => {
+    const path = ({
+      info: this.#infoPath,
+      error: this.#errorPath
+    })[type]
 
-      return promisify(fs.appendFile)(this.#infoPath, message)
-    },
-    error: async error => {
-      if (typeof this.#errorPath !== 'string') return
+    if (typeof path !== 'string') return
 
-      return promisify(fs.appendFile)(this.#errorPath, error)
-    }
+    return promisify(fs.appendFile)(path, JSON
+      .stringify(data, null, INDENT) + ",\n") // eslint-disable-line quotes
   }
 
   /**
@@ -59,114 +72,160 @@ export default class Logger extends EventEmitter {
    * Log information
    *
    * @param {string} [scope]
-   * @param {...any} messages
+   * @param {...*} [data]
    *
-   * @return {(object|undefined)} Returns produced object or undefined if messages parameter left empty
+   * @return {object}
+   *
+   * @see module:logger.createInfoObject
    */
-  info (scope, ...messages) {
-    if (typeof this.#infoPath !== 'string') return
+  info (scope, ...data) {
+    const infoObject = createInfoObject(scope, ...data)
 
-    const message = Object.create(null)
+    if (infoObject !== undefined) this.#append('info', infoObject)
 
-    if (typeof scope !== 'string') messages.unshift(scope)
-
-    if (!messages.length) return
-
-    const messagesMustRemove = []
-
-    for (const messageId in messages) {
-      if (messages[messageId][logSymbol] !== undefined) {
-        messages[messageId] = messages[messageId][logSymbol]
-      }
-
-      if (messages[messageId] instanceof Object &&
-        !(messages[messageId] instanceof Array)) {
-        messagesMustRemove.unshift(messageId)
-
-        Object.assign(message, messages[messageId])
-      }
-    }
-
-    for (const messageId of messagesMustRemove) {
-      messages.splice(messageId, 1)
-    }
-
-    message.messages = messages.length ? messages : undefined
-
-    Object.assign(message, {
-      scope: typeof scope === 'string' ? scope : undefined,
-      date: Date(Date.now())
-    })
-
-    this.#logger.info(JSON.stringify(message, null, 2) + ',')
-
-    return message
+    return infoObject
   }
 
   /**
    * Log errors
    *
    * @param {string} [scope]
-   * @param {...any} errors
+   * @param {...*} [data]
    *
-   * @return {(object|undefined)} Returns produced object or undefined if errors parameter left empty
+   * @return {object}
+   *
+   * @see module:logger.createErrorObject
    */
-  error (scope, ...errors) {
-    if (typeof this.#errorPath !== 'string') return
+  error (scope, ...data) {
+    const infoObject = createErrorObject(scope, ...data)
 
-    const error = Object.create(null)
+    if (infoObject !== undefined) this.#append('error', infoObject)
 
-    if (typeof scope !== 'string') errors.unshift(scope)
-
-    if (!errors.length) return
-
-    const errorsMustRemove = []
-
-    for (const errorId in errors) {
-      if (errors[errorId][logSymbol] !== undefined) {
-        errors[errorId] = errors[errorId][logSymbol]
-      }
-
-      if (errors[errorId] instanceof Error) {
-        errorsMustRemove.unshift(errorId)
-
-        const _error = errors[errorId]
-        const result = Object.create(null)
-
-        Object.assign(result, _error)
-
-        for (const key of Object.getOwnPropertyNames(Object.getPrototypeOf(_error))) {
-          result[key] = _error[key]
-        }
-
-        for (const key of Object.getOwnPropertyNames(_error)) {
-          result[key] = _error[key]
-        }
-
-        Object.assign(error, result)
-      } else if (errors[errorId] instanceof Object &&
-        !(errors[errorId] instanceof Array)) {
-        errorsMustRemove.unshift(errorId)
-
-        Object.assign(error, errors[errorId])
-      }
-    }
-
-    for (const errorId of errorsMustRemove) {
-      errors.splice(errorId, 1)
-    }
-
-    error.errors = errors.length ? errors : undefined
-
-    Object.assign(error, {
-      scope: typeof scope === 'string' ? scope : undefined,
-      date: Date(Date.now())
-    })
-
-    this.#logger.error(JSON.stringify(error, null, 2) + ',')
-
-    return error
+    return infoObject
   }
+}
+
+/**
+ * @summary Create an object based inputs for logging information
+ * @description
+ * Adds date key to return object with current human readable date and time <br>
+ * Use object's `{@link module:logger.logSymbol}` getter instead object if available
+ *
+ * @param {string} [scope] If this parameter provided, added to return object by "scope" key
+ * @param {...*} [data] All objects merged with return object and other primitive values push to an array that its key is "messages"
+ *
+ * @return {object}
+ */
+export function createInfoObject (scope, ...data) {
+  const infoObject = Object.create(null)
+
+  if ((scope !== undefined &&
+    typeof scope !== 'string') ||
+    (!data.length &&
+      typeof scope === 'string')) {
+    data.unshift(scope)
+
+    scope = undefined
+  }
+
+  if (!data.length) return
+
+  const mergedObjects = []
+
+  for (const dataIndex in data) {
+    if ((data[dataIndex] && data[dataIndex][logSymbol]) !== undefined) {
+      data[dataIndex] = data[dataIndex][logSymbol]
+    }
+
+    if (typeof data[dataIndex] === 'object' &&
+      !(data[dataIndex] instanceof Array)) {
+      mergedObjects.unshift(dataIndex)
+
+      Object.assign(infoObject, data[dataIndex])
+    }
+  }
+
+  for (const objectIndex of mergedObjects) {
+    data.splice(objectIndex, 1)
+  }
+
+  infoObject.messages = data.length ? data : undefined
+
+  Object.assign(infoObject, {
+    scope: typeof scope === 'string' ? scope : undefined,
+    date: Date(Date.now())
+  })
+
+  return infoObject
+}
+
+/**
+ * @summary Create an object based inputs for logging errors
+ * @description
+ * Same as {@link module:logger.createInfoObject} with some additional behaviors <br><br>
+ * Errors pushes to "_errors" array for future usage <br>
+ * Errors serialize to normal objects <br>
+ * If Number of errors equal to 1, then it sets to "error" key, else errors pushes to "errors" key
+ *
+ * @param {string} [scope]
+ * @param {...*} [data]
+ *
+ * @return {object}
+ *
+ * @see module:logger.createInfoObject
+ */
+export function createErrorObject (scope, ...data) {
+  if ((scope !== undefined &&
+    typeof scope !== 'string') ||
+    (!data.length &&
+      typeof scope === 'string')) {
+    data.unshift(scope)
+
+    scope = undefined
+  }
+
+  if (!data.length) return
+
+  const errors = []
+
+  for (let dataIndex = data.length - 1; dataIndex >= 0; dataIndex--) {
+    if (data[dataIndex] instanceof Error) errors.unshift(data.splice(dataIndex, 1)[0])
+  }
+
+  for (const error of errors) {
+    if (error[logSymbol] !== undefined) data.push(error[logSymbol])
+  }
+
+  if (!data.length) data.push(Object.create(null))
+
+  const errorObject = createInfoObject(scope, ...data)
+
+  if (errorObject === undefined) return
+
+  Object.defineProperty(errorObject, '_errors', {
+    value: errors.length ? errors : undefined,
+    enumerable: false
+  })
+
+  const safeErrors = []
+
+  if (errorObject._errors) {
+    for (const error of errors) {
+      const safeError = safeErrors[safeErrors.push(Object.create(null)) - 1]
+
+      for (const key of Object.getOwnPropertyNames(Object.getPrototypeOf(error))) {
+        safeError[key] = error[key]
+      }
+      for (const key of Object.getOwnPropertyNames(error)) {
+        safeError[key] = error[key]
+      }
+    }
+  }
+
+  if (safeErrors.length === 1) errorObject.error = safeErrors[0]
+  else if (safeErrors.length > 1) errorObject.errors = safeErrors
+
+  return errorObject
 }
 
 /**
