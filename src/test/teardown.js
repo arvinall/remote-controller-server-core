1a341c8ad3e4e1a1eea19e879f3f04670d7d296f
Arvinall <arvinall021@gmail.com>
2019-06-26 01:27:08 +0430

    Remove log files after tests done based on environment LOG variable

--- src/test/teardown.js
+++ src/test/teardown.js
@@ -2,13 +2,24 @@
 import path from 'path'
 import { promisify } from 'util'
 import fs from 'fs'
+import envConfigs from './configs'
 
 const TMP_PATH = path.join(__dirname, 'tmp')
 const idHolderName = path.join(TMP_PATH, 'lastId.tmp')
+const logFilesName = [
+  path.join(TMP_PATH, 'info.log'),
+  path.join(TMP_PATH, 'error.log')
+]
 
 export default async function teardown () {
   // Remove last id holder file
   try { await promisify(fs.unlink.bind(fs))(idHolderName) } catch (e) {}
+  // Remove log files
+  if (!envConfigs.log) {
+    for (const file of logFilesName) {
+      try { await promisify(fs.unlink.bind(fs))(file) } catch (e) {}
+    }
+  }
   // Remove temporary directory
   try { await promisify(fs.rmdir.bind(fs))(TMP_PATH) } catch (e) {}
 }
