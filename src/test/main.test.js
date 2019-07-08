d869983f5f52a6a89451ed758624088a8275f825
Arvinall <arvinall021@gmail.com>
2019-07-08 19:26:17 +0430

    Add logger, connections module type check

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -6,8 +6,10 @@ const serverCore = makeCore({ storagePath: TMP_PATH })
 
 test('Core must return core module correctly', () => {
   expect(serverCore).toEqual(expect.objectContaining({
+    logger: expect.any(Object),
     storages: expect.any(Object),
     preferences: expect.any(Object),
+    connections: expect.any(Object),
     engine: expect.any(Object)
   }))
 })
